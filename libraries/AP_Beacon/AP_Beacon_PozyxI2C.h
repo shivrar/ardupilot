a8b7483043e4fe0b0a6335d51606e51206811a24
shivan <s_ram_dhanie@hotmail.com>
2020-07-14 17:27:48 +0100

    Initial commit containing Pozyx definitions, header and source backends

---
+++ libraries/AP_Beacon/AP_Beacon_PozyxI2C.h
@@ -0,0 +1,16 @@
+//
+// Created by shivan on 7/14/20.
+//
+/*
+    This work documented consists part of the MSc. Robotics Dissertation.
+ */
+#pragma once
+
+#include "AP_Beacon_Backend.h"
+#include "AP_Pozyx_definitions.h"
+#include <AP_HAL/I2CDevice.h>
+
+class AP_Beacon_PozyxI2C : public AP_Beacon_Backend
+{
+
+};
