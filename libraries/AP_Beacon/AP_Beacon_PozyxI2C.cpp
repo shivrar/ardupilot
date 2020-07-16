38abd55fbe3a1c1e6d2d1f030b8ae7ec45b386a7
shivan <s_ram_dhanie@hotmail.com>
2020-07-16 18:22:59 +0100

    Initial stub of example file with steps to complete

--- libraries/AP_Beacon/AP_Beacon_PozyxI2C.cpp
+++ libraries/AP_Beacon/AP_Beacon_PozyxI2C.cpp
@@ -2,3 +2,7 @@
 // Created by shivan on 7/14/20.
 //
 
+#include "AP_Beacon_PozyxI2C.h"
+
+// This is a directive declaration so we'll grab hal from the relevant definition file
+extern const AP_HAL::HAL& hal;
---
+++ libraries/AP_Beacon/examples/AP_Pozyx_test/AP_pozyx_test_i2c.cpp
@@ -0,0 +1,33 @@
+/*
+    Created by shivan on 7/15/20.
+    Simple i2c ifc test for the pozyx tag.
+    This script should reproduce the functionality of the ready_to_localise.ino file provided by the Pozyx Arduino
+    library. However it will use the library ifc being built into the ardupilot instead.
+
+*/
+
+#include <AP_HAL/AP_HAL.h>
+#include <AP_HAL/I2CDevice.h>
+#include <AP_Beacon_PozyxI2C.h>
+
+const AP_HAL::HAL& hal = AP_HAL::get_HAL();
+//We can hardcode the anchor positions here for the time being
+//TODO: Look at putting the the anchors configurable via parameters. Possibly reuse the memory for the current ones.
+void setup();
+void loop();
+
+void setup(void)
+{
+    // Setup the I2C bus and send the configuration data for the tag
+    /* Required functionality to build into the library:
+     * -Connect to bus and return checkable data if successful or not.
+     * -Configure the pozyx tag with the relvenat acnhor, algorithm, dimension etc.
+     * */
+}
+
+void loop(void)
+{
+    // Do positioning periodically and serial print it -> we can look at this using a serial moniter or mavproxy
+}
+
+AP_HAL_MAIN();
