b5789c0523527f7853bf22a7282d916fe384585d
shivan <s_ram_dhanie@hotmail.com>
2020-07-16 18:22:59 +0100

    Initial stub of example file with steps to complete

--- libraries/AP_Beacon/AP_Beacon_PozyxI2C.h
+++ libraries/AP_Beacon/AP_Beacon_PozyxI2C.h
@@ -12,5 +12,21 @@
 
 class AP_Beacon_PozyxI2C : public AP_Beacon_Backend
 {
+public:
+    // Constructor
+    AP_Beacon_PozyxI2C(AP_Beacon &frontend, AP_HAL::OwnPtr<AP_HAL::I2CDevice> dev)
 
+    // return true if sensor is basically healthy (we are receiving data)
+    bool healthy() override;
+
+    // update
+    void update(void) override;
+
+private:
+    struct Coordinates{
+        // Coordinate data will be stored as mm
+        int32_t x;
+        int32_t y;
+        int32_t z;
+    };
 };
