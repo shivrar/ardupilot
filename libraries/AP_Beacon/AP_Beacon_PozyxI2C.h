e07ade1336aee40f9cd7704dcb81a50fcb4eb48c
shivan <s_ram_dhanie@hotmail.com>
2020-07-16 22:35:40 +0100

    wip

--- libraries/AP_Beacon/AP_Beacon_PozyxI2C.h
+++ libraries/AP_Beacon/AP_Beacon_PozyxI2C.h
@@ -3,6 +3,8 @@
 //
 /*
     This work documented consists part of the MSc. Robotics Dissertation.
+     Much of the ifc would be based on reading specific registers of the Pozyx tag since most of the processing is
+    already done onboard.
  */
 #pragma once
 
@@ -22,11 +24,24 @@ public:
     // update
     void update(void) override;
 
+    // Setup the relevant stuffies for the Pozyx tag
+    void init(void);
+
+    // Would need to add a anchor which would need the position (x,y,z) and the ID
+    void add_device(const Vector3& position, uint16_t tag_id);
+
+
 private:
     struct Coordinates{
         // Coordinate data will be stored as mm
         int32_t x;
         int32_t y;
         int32_t z;
+        uint32_t timestamp;
     };
+
+    // Lets have a member variable to store the hal i2c ifc
+    AP_HAL::OwnPtr<AP_HAL::I2CDevice> _dev;
 };
+
+//
